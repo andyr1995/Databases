d7269a4db2c9912e4c74778442dbe41c00c63ac4
Andrew Roberts <andyr1995@live.co.uk>
2022-02-17 12:04:17 +0000

    First upload of current database

---
+++ demoWORLD.sql
@@ -0,0 +1,5 @@
+INSERT INTO mydb.record_label (company_name, company_id, company_location) VALUES (" ",1, "  ");
+SELECT * FROM mydb.record_label;
+SELECT artist_name, record_label_company_id FROM mydb.artist;
+
+
---
+++ demodb.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movie ( 
+Movie_ID int,
+Duration int,
+Movie_name varchar(255),
+Age_Rating int );
+INSERT INTO movie
+VALUES (1,2,3,4,5,6,7,8,9);
+
+
